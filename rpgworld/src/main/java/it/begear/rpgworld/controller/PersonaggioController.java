f337b91da61f281647bbbe21529d0a2b9fcf21a0
Federicodecesare <74314615+Federicodecesare@users.noreply.github.com>
2020-12-03 15:51:09 +0100

    controller

--- rpgworld/src/main/java/it/begear/rpgworld/controller/PersonaggioController.java
+++ rpgworld/src/main/java/it/begear/rpgworld/controller/PersonaggioController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import it.begear.rpgworld.entity.Personaggio;
 import it.begear.rpgworld.service.PersonaggioService;
@@ -31,5 +33,12 @@ public class PersonaggioController {
 		personaggioService.save(personaggio);
 		return "redirect:/";
 	}
+	@RequestMapping("/delete/{id}")
+	public String deletePersonaggio(@PathVariable(name ="id")int id)
+	{
 
+		personaggioService.delete(id);
+		return "redirect:/";
+		
+	}
 }
